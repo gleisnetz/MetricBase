d915f8e5725b5be2e3385f7496420f88f53d9704
Josef Eisensamer <gleisnetz@icloud.com>
2025-07-15 09:55:39 +0200

    Initial Commit

---
+++ MetricBase/MetricBaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricBaseApp.swift
+//  MetricBase
+//
+//  Created by Josef Eisensamer on 15.07.25.
+//
+
+import SwiftUI
+
+@main
+struct MetricBaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MetricBaseTests/MetricBaseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MetricBaseTests.swift
+//  MetricBaseTests
+//
+//  Created by Josef Eisensamer on 15.07.25.
+//
+
+import Testing
+@testable import MetricBase
+
+struct MetricBaseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
